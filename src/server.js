ec34b531f65a18783281c63fcb748c7d3a8f6fa6
martin <manirumva@gmail.com>
2026-06-30 08:06:36 +0200

    Server.js file

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+// Database connection
+const start = async () => {
+  try {
+    // await sequelize.authenticate();
+    // console.log("Database connected!!");
+
+    // await sequelize.sync();
+    // // await sequelize.sync({ alter: true });
+    // console.log("Models synced!");
+
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (error) {
+    console.error("DB connection failed!:", error);
+  }
+};
+
+start();
